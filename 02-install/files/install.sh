9d2d3d9594dc6008bf576ef084b3f98198c0094b
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-04-05 11:19:26 -0500

    Remove an extra tar extraction from the install script and fix some issues with the Dockerfile wget and tar extraction.

--- 02-install/files/install.sh
+++ 02-install/files/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 vncserver :0
-tar -xzf Vivado_2018.2.tar.gz -C /opt/
 /opt/Vivado_2018.2/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
