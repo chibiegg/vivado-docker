de935099df5bc501e4ab8932ea55767a950fce16
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-08-30 08:37:49 -0500

    Update to Xilinx Vivado 2019.1. Also add some space saving commands.

--- 02-install/files/install.sh
+++ 02-install/files/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 vncserver :0
-/opt/Vivado_2018.2/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
+/opt/Xilinx_Vivado_SDK_2019.1_0524_1430/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
--- 03-setup/files/entrypoint.sh
+++ 03-setup/files/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 vncserver -geometry ${GEOMETRY} :0
-/opt/Xilinx/Vivado/2018.2/bin/vivado
+/opt/Xilinx/Vivado/2019.1/bin/vivado
