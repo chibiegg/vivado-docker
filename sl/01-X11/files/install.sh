b99d44801e201fc721987e37c95ac6e0b4d73429
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-09-13 17:10:30 -0500

    Re-arrange all of the packages so that the project can be built against multiple OS and with multiple levels of viewing (i.e. vnc and/or X11). Currently the Ubuntu package is built only with both X11 and VNC and the SL package is built only with X11.

---
+++ sl/01-X11/files/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/Xilinx_Vivado_SDK_2019.1_0524_1430/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
--- ubuntu/01-vnc/files/entrypoint.sh
+++ ubuntu/01-vnc/files/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 vncserver -geometry ${GEOMETRY} :0
+#export DISPLAY=$HOSTNAME:0
 /opt/Xilinx/Vivado/2019.1/bin/vivado
