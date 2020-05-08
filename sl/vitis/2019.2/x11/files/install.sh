cd2c25b1387a1c0acf15a96fc0d88339367d0468
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-05-08 17:24:44 -0500

    Fix some incorrect version numbers.

--- sl/vitis/2019.2/x11/files/install.sh
+++ sl/vitis/2019.2/x11/files/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/Xilinx_Vitis_2019.2_1024_1831/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
+/opt/Xilinx_Vitis_2019.2_1106_2127/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config /opt/install_config.txt
